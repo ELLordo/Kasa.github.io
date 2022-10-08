fdfe09a3005e1d2d553db9c14be246908d1998e5
ELLordo <104216845+ELLordo@users.noreply.github.com>
2022-10-08 19:33:10 +0200

    Page Home fini

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import BannerHome from "../component/Asset/BannerHome.svg"
+import PropTypes from 'prop-types'
+import BDD from "../component/JSON/BDD"
+
 
 const ImgHome= styled.img`
 border-radius: 25px;
@@ -15,27 +18,100 @@ justify-content:center;margin:60px;
 
 const TitreHome = styled.h1`
 position:absolute;
-
 font-family: 'Montserrat';
 font-style: normal;
 font-weight: 500;
 font-size: 48px;
 line-height: 142.6%;
-left: 30%;
-right: 25%;
-top: 25%;
+
+margin-top: 6%;
 color: #FFFFFF;
 `
+const CardGeneral= styled.div`
+display:flex;
+flex-flow:row wrap;
+columns : auto 3;
+justify-content:space-evenly;
+`
+
+const CardSpe = styled.div`
+display:flex;
+flex-flow:column;
+height:400px;
+width: 27%;
+margin:10px 10px 40px 10px;
+background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%);
+border-radius:10px;
+`
+const Appartement = BDD
+
+const CardImg = styled.img`
+display:flex;
+width:100%;
+height:100%;
+border-radius:10px
+`
+const CardFiltre = styled.div`
+display:flex;
+position:absolute;
+height:400px;
+width: 26.8%;
+background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%);
+border-radius:10px;
+`
+const CardTitle = styled.span`
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 142.6%;
+display: flex;
+align-items: flex-end;
+width:200px;
+color: #FFFFFF;
+margin-top:-60px;
+z-index:1
+`
+
+function Card({id, cover, title}) {
+    return (
+        <CardSpe>
+          
+            <CardImg src={cover} alt="freelance" height={340} width={340} />
+            
+            <CardTitle>{title}</CardTitle><CardFiltre />
+        </CardSpe>
+    )
+}
+
+Card.propTypes = {
+    label: PropTypes.string,
+    title: PropTypes.string,
+    picture: PropTypes.string,
+}
 
 function Home() {
   return (
-    
+    <div>
+
       <DivHome>
         <ImgHome src={BannerHome} alt="Paysage de presentation" />
         <TitreHome>Chez vous, partout et ailleurs</TitreHome>
       </DivHome>
-    
 
+      <CardGeneral>
+        
+        {Appartement.map((profile) => (
+                <Card
+                    key={`${profile.id}`}
+                    cover={profile.cover}
+                    title={profile.title}
+                />
+            ))}
+        
+      </CardGeneral>
+
+    </div>
   );
 }
 
