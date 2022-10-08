191e1a528e9dd496037934f2ac69474243ff726c
ELLordo <104216845+ELLordo@users.noreply.github.com>
2022-10-08 16:31:26 +0200

    Header et footer fini avec Css

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,16 +1,45 @@
 import LogoF from "../Asset/LogoF.svg"
 import styled from 'styled-components'
 
-const BackImg = styled.div`
-background-color: #000000;`
+const BackDiv = styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+flex-flow:column;
+background-color: #000000;
+Width :100%;
+Height:209px;
+
+`
+
+const BackImg =styled.img`
+
+display: flex;
+justify-content: center;
+width: 122px;
+height: 40px;
+padding-top:40px;
+`
+const FooterText = styled.p`
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 24px;
+line-height: 142.6%;
+display: flex;
+align-items: flex-end;
+text-align: center;
+color: #FFFFFF;
+`
 
 function Footer() {
   return (
-    <BackImg>
+    <BackDiv>
         
-      <img src={LogoF} alt="Logo Kasa" />
-      
-    </BackImg>
+      <BackImg src={LogoF} alt="Logo Kasa" />
+      <FooterText>© 2020 Kasa. All rights reserved</FooterText> 
+    </BackDiv>
   );
 }
 
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,48 @@
 import LogoP from "../Asset/LogoP.svg"
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const ImgHeader = styled.img`
+padding-top:40px;
+padding-left:40px`
+
+const LinkHeader = styled(Link)`
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 24px;
+line-height: 142.6%;
+
+display: flex;
+flex-flow:row;
+text-decoration:none;
+align-items: flex-end;
+text-align: right;
+
+color: #FF6060;
+`
+const NavHeader= styled.nav`
+display:flex;
+flex-flow:row;
+justify-content: space-evenly;
+box-sizing: border-box;
+width: 25%;
+`
+const HeaderDiv=styled.div`
+display:flex;
+flex-flow:row;
+justify-content:space-between;
+`
 
 function Header() {
   return (
-    <div >
-      <img src={LogoP} alt="Logo Kasa" />
-    </div>
+    <HeaderDiv >
+      <ImgHeader src={LogoP} alt="Logo Kasa" />
+      <NavHeader>
+      <LinkHeader to="/">Accueil</LinkHeader>
+      <LinkHeader to="/apropos">A Propos</LinkHeader>
+      </NavHeader>
+    </HeaderDiv>
   );
 }
 
